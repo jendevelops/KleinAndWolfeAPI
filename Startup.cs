11615cafece2af21c3248d735c733048ca6bf265
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-12 12:17:48 -0800

    Add settings for database in setup and appsettings

--- Startup.cs
+++ Startup.cs
@@ -1,32 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using kleinandwolfeapi.Models;
 
 namespace kleinandwolfeapi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+    public Startup(IConfiguration configuration, IHostingEnvironment env)
+    {
+      if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+      {
+        Configuration = configuration;
+      }
+      else
+      {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json");
+        Configuration = builder.Build();
+      }
+    }
 
-        public IConfiguration Configuration { get; }
+
+    public IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-        }
+      if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+        services.AddEntityFrameworkSqlServer()
+        .AddDbContext<ApplicationDbContext>(options => options
+        .UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+      else
+      {
+        services.AddEntityFrameworkMySql()
+            .AddDbContext<ApplicationDbContext>(options => options
+            .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+      }
+
+      services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
+
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+          .AddEntityFrameworkStores<ApplicationDbContext>()
+          .AddDefaultTokenProviders();
+
+      services.Configure<IdentityOptions>(options =>
+      {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredUniqueChars = 0;
+      });
+
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -41,7 +80,7 @@ namespace kleinandwolfeapi
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }

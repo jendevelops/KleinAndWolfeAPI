3da5b51f1b9a1e6591800af06157354e676e4b88
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-15 22:44:46 -0800

    Add list of products associated with client

--- Models/Client.cs
+++ Models/Client.cs
@@ -7,6 +7,11 @@ namespace kleinandwolfeapi.Models
     public int Name {get;set;}
     public int PartnerId{get;set;}
     public virtual Partner Partner {get;set;}
+    public virtual ICollection<Product> Products {get;set;}
+
+    Client(){
+      Products = new HashSet<Product>();
+    }
   }
   
 }

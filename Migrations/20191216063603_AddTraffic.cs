ccae6152968866bdb54125f9dc066ebf17a8b05e
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-15 22:47:14 -0800

    New migration with SiteTraffic table added

---
+++ Migrations/20191216063603_AddTraffic.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace kleinandwolfeapi.Migrations
+{
+    public partial class AddTraffic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "SiteTraffic",
+                columns: table => new
+                {
+                    TrafficId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Location = table.Column<int>(nullable: false),
+                    OrderId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SiteTraffic", x => x.TrafficId);
+                    table.ForeignKey(
+                        name: "FK_SiteTraffic_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ClientId",
+                table: "Products",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SiteTraffic_OrderId",
+                table: "SiteTraffic",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Clients_ClientId",
+                table: "Products",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "ClientId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Clients_ClientId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "SiteTraffic");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ClientId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Products");
+        }
+    }
+}

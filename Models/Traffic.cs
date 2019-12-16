1daed3aab76f87d1bc2fd73b6b819f195725840a
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-15 23:42:22 -0800

    Add Date and ClientId fileds

--- Models/Traffic.cs
+++ Models/Traffic.cs
@@ -1,10 +1,12 @@
-using System.Collections.Generic;
+using System;
 
 namespace kleinandwolfeapi.Models
 {
   public class Traffic
   {
     public int TrafficId { get; set; }
+    public int ClientId {get;set;}
+    public DateTime Date {get;set;}
     public int Location { get; set; }
     public Order Order {get;set;}
   }

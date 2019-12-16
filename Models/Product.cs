9c7d188580e5a5058f829890e22b8b1db4063c4f
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-12-15 22:45:28 -0800

    Add client reference to product

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,6 +5,8 @@ namespace kleinandwolfeapi.Models
   public class Product
   {
     public int ProductId { get; set; }
+    public int ClientId {get;set;}
+    public virtual Client Client {get;set;}
     public string Name { get; set; }
     public string Description { get; set; }
 
